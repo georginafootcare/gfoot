b7b5d9dc18200a732033f7e81585765978a3f9e9
wightryder <github@solentsoftware.co.uk>
2026-03-23 15:45:18 +0000

    gfoot V4.0 Framework changes

--- js/gfoot-main.js
+++ js/gfoot-main.js
@@ -3,21 +3,36 @@ class SiteFooter extends HTMLElement {
         this.innerHTML = `<div class="item-footer">
         <p><strong>Call 07807 457069 for mobile foot care on the Isle of Wight.</strong></p>
         <p>&copy; 2025 Georgina Painter BSc (Hons) Podiatry. All&nbsp;rights&nbsp;reserved.</p>
-        <p>HCPC&nbsp;Registered&nbsp;No&nbsp;CH33419. (Ref js)</p>
-    </div>`
-        const script = document.createElement('script')
-        script.dataset.goatcounter = 'https://georginafoorcare.goatcounter.com/count'
-        script.src = '//gc.zgo.at/count.js'
-        script.async = true
-        document.head.appendChild(script)
+        <p>HCPC&nbsp;Registered&nbsp;No&nbsp;CH33419</p>
+    </div>`;
     }
 }
-customElements.define('site-footer', SiteFooter)
 
-const fabs = document.querySelectorAll('.fab')
+customElements.define('site-footer', SiteFooter);
+
+const fabs = document.querySelectorAll('.fab');
 
 window.addEventListener('scroll', () => {
     fabs.forEach(fab => {
-        fab.classList.toggle('fab--visible', window.scrollY > 300)
-    })
-})
+        fab.classList.toggle('fab--visible', window.scrollY > 300);
+    });
+});
+
+// Turnstile callback (Cloudflare calls this automatically)
+window.turnstileReady = function (token) {
+    const btn = document.getElementById("submit-btn");
+
+    if (btn) {
+        btn.disabled = false;
+        btn.classList.add("enabled");
+    }
+};
+
+// GoatCounter analytics
+const gc = document.createElement('script')
+gc.dataset.goatcounter = 'https://georginafootcare.goatcounter.com/count'
+gc.src = '//gc.zgo.at/count.js'
+gc.async = true
+document.head.appendChild(gc)
+
+
